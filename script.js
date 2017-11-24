3fdad258c5d821a06fcaeb9ad231cea0b4b904aa
Chloe Lente <lente.chloe@gmail.com>
2017-11-24 14:43:04 +0100

    Fin de la correction

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 //on crée une variable vildForm qui utilise Angular.
 //mailUtils=nom du module.
 //ngRoute=dépendance.
-var validForm = angular.module('mailUtils', ['ngRoute']);
-//j'utilise le routeProvider
+var validForm = angular.module('mailUtils', ['ngRoute']); //déclarer un nouveau module
+//.run permet de faire une action au démarrage de l'application
+validForm.run(function($rootScope){
+  //rootScope = variable globale qui passe outre les controllers
+  //on initialise des tableaux ici pour eviter d'écraser les données au chargement de la page
+  $rootScope.subjectList=[];
+  $rootScope.nameList=[];
+  $rootScope.emailList=[];
+  $rootScope.textList=[];
+});
+//configuration des route
 validForm.config(['$routeProvider', function($routeProvider){
   $routeProvider
 //quand je click sur form je vais chercher mon fichier form.html et utilise mon controller formControl
@@ -11,7 +20,7 @@ validForm.config(['$routeProvider', function($routeProvider){
     controller:'formControl'
   })
   // ? veut dire que c'est optionnel
-  .when('/view/:subjectName?', {
+  .when('/view/:id?', {
     templateUrl:'partials/view.html',
     controller:'viewControl'
   })
@@ -21,16 +30,22 @@ validForm.config(['$routeProvider', function($routeProvider){
   });
 }]);
 //Je crée mon controller formControl
-validForm.controller('formControl',['$scope', function($scope){
-  $scope.subjectList=[];
+validForm.controller('formControl',['$scope', '$rootScope', function($scope, $rootScope){
   //j'appelle ma fonction sendClick du bouton Envoyer
   $scope.sendClick=function(){
-    //push ermet de récupérer tous les sujets du tableau subjectList
-    $scope.subjectList.push($scope.subject);
+    //push permet de récupérer tous les sujets du tableau subjectList
+    $rootScope.subjectList.push($scope.subject);
+    $rootScope.nameList.push($scope.name);
+    $rootScope.emailList.push($scope.email);
+    $rootScope.textList.push($scope.text);
   }
 }]);
 //je crée mon controller viewControl
-validForm.controller('viewControl', ['$scope', '$routeParams', function($scope, $routeParams){
-//routeParams récupère toutes les variables de subjectName
-  $scope.subjectName=$routeParams.subjectName;
+validForm.controller('viewControl', ['$scope', '$routeParams', '$rootScope', function($scope, $routeParams, $rootScope){
+//routeParams récupère toutes les variables de id
+  $scope.id=$routeParams.id;
+  $scope.subject=$rootScope.subjectList[$scope.id];
+  $scope.name=$rootScope.nameList[$scope.id];
+  $scope.email=$rootScope.emailList[$scope.id];
+  $scope.text=$rootScope.textList[$scope.id];
 }]);
